28bb779bc0be4846f4e85e4a7d70cb8a1fed3981
aiden <github@sina.cn>
2023-04-04 15:29:37 +0800

    fix: get debounce value

--- packages/plugin-debounce/src/index.ts
+++ packages/plugin-debounce/src/index.ts
@@ -20,19 +20,19 @@ export default definePrierPlugin<IDebouncePluginOptions>({
       const requestConfig = req.getConfig();
       requestConfig.debounce = requestConfig.debounce === undefined ? config.debounce : requestConfig.debounce;
       // 配置了防抖时间
-      if (config.debounce > 0) {
+      if (requestConfig.debounce > 0) {
         const token = req.getToken();
         const now = Date.now();
         const lastTime = state.get(token);
         // 判断是否还在防抖时间内
-        if (lastTime && now - lastTime < config.debounce) {
+        if (lastTime && now - lastTime < requestConfig.debounce) {
           return res.send(new Error("debounce"));
         }
         state.set(token, now);
         setTimeout(() => {
           // 防抖时间结束后删除token
           state.delete(token);
-        }, config.debounce);
+        }, requestConfig.debounce);
       }
       // 执行下一个插件
       return req.next();
